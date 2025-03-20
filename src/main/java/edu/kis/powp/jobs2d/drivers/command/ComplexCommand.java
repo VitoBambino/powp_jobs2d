d29a0ac826fd5f686451c6044fc8c66ee55487b5
Witold <witek99.pl@gmail.com>
2025-03-20 13:32:05 +0100

    implementation of Comlexcommand class

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/ComplexCommand.java
@@ -6,8 +6,12 @@ public class ComplexCommand implements DriverCommand {
 
     List<DriverCommand> commands;
 
+    ComplexCommand(List<DriverCommand> commands) {
+        this.commands = commands;
+    }
+
     @Override
     public void execute() {
-
+        this.commands.forEach(DriverCommand::execute);
     }
 }
